35f0ea9a409ad974b749678b7437462d8b836a64
heumminJo <chm9709@naver.com>
2021-01-13 09:37:34 +0900

    FIX

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://stickershop.line-scdn.net/stickershop/v1/product/15492/LINEStorePC/main.png;compress=true" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Pi Ka ~
         </p>
         <a
           className="App-link"
